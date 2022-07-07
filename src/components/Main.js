6931aa372eb0abd922c68eff8b4d878d79127368
David Schepis <davidschepis@gmail.com>
2022-07-06 19:50:38 -0700

    added 3 cards to main comp

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,49 @@
 import React, { useState } from "react";
 
 const cardStyle = {
-    width: "18rem"
+    border: "solid rgb(199, 199, 199) 2px",
+    display: "flex",
+    overflow: "hidden",
+    borderRadius: "5px",
+};
+
+const imgStyle = {
+    borderRadius: "2px",
+    maxHeight: "500px"
 };
 
 const Main = () => {
     return (
-        <div className="">
-            <div className="col-4">
-                <div className="card" style={cardStyle}>
-                    <img className="card-img-top" src="..." alt="Card image cap"/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
+        <div className="row">
+            <div className="col-4 text-center">
+                <div className="card h-100" style={cardStyle}>
+                    <img className="card-img-top" src="images/metronome.png" alt="Metronome" style={imgStyle} />
+                    <div className="card-body">
+                        <h5 className="card-title">Metronome</h5>
+                        <p className="card-text">Modify BPM as needed</p>
+                        <a href="#" className="btn btn-primary">Open</a>
+                    </div>
                 </div>
             </div>
-            <div className="col-4">
-
+            <div className="col-4 text-center">
+                <div className="card h-100" style={cardStyle}>
+                    <img className="card-img-top" src="images/drums.png" alt="Metronome" style={imgStyle} />
+                    <div className="card-body">
+                        <h5 className="card-title">Beat Creator</h5>
+                        <p className="card-text">Create a basic drum loop</p>
+                        <a href="#" className="btn btn-primary">Open</a>
+                    </div>
+                </div>
             </div>
-            <div className="col-4">
-
+            <div className="col-4 text-center">
+                <div className="card h-100" style={cardStyle}>
+                    <img className="card-img-top" src="images/note.png" alt="Metronome" style={imgStyle} />
+                    <div className="card-body">
+                        <h5 className="card-title">Note Finder</h5>
+                        <p className="card-text">Find all notes in selected key</p>
+                        <a href="#" className="btn btn-primary">Open</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
